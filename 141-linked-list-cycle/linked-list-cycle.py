13f56d6efbed72553c455357a5a42a74539e6861
Muhammad Nabeel <nabeelmdnabeel1229@gmail.com>
2026-07-29 20:10:03 +0530

    Time: 70 ms (5.94%) | Memory: 23.1 MB (7.48%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        cycle = set()
+        while head:
+            if head in cycle:
+                return True
+            cycle.add(head)
+            head = head.next 
+        return False    
